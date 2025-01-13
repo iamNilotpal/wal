ec61710915ddf13b384de5016e9b0ddc5c36f3d9
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2025-01-13 22:58:33 +0530

    Read config file from command line flag.

--- cmd/wal/main.go
+++ cmd/wal/main.go
@@ -1,8 +1,33 @@
 package main
 
-import "github.com/iamNilotpal/wal/pkg/logger"
+import (
+	"flag"
+
+	"github.com/iamNilotpal/wal/config"
+	"github.com/iamNilotpal/wal/pkg/logger"
+)
 
 func main() {
 	logger := logger.New("wal-service")
-	logger.Info("starting wal service")
+	defer logger.Sync()
+
+	logger.Info("Starting wal service")
+
+	path := flag.String("config", "", "Config file path")
+	flag.Parse()
+
+	var cfg *config.Config
+
+	if path == nil || *path == "" {
+		cfg = config.DefaultConfig()
+		logger.Infow("Using default config", "config", cfg)
+	} else {
+		c, err := config.LoadConfig(*path)
+		if err != nil {
+			logger.Fatalw("Error loading config", "path", *path, "error", err)
+		}
+
+		cfg = c
+		logger.Infow("Config loaded successfully", "config", cfg)
+	}
 }
